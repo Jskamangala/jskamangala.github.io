928ff8cf324ec5606be114fb6f6ad5381414e826
Jskamangala <jskamangala@gmail.com>
2026-08-20 10:45:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector('.menu-toggle');
+const links = document.querySelector('.nav-links');
+toggle.addEventListener('click', () => links.classList.toggle('open'));
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => links.classList.remove('open'));
+});
